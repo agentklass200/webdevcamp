726a33f556e6a94ec9bb1f6f7d66246ac43d9f74
Alecxis Banag <alecxisbanag@gmail.com>
2017-06-19 11:10:39 +0000

    Finished NPM faker excercise

---
+++ npm-faker/listProduct.js
@@ -0,0 +1,54 @@
+var faker = require("faker");
+var cat = require("cat-me");
+
+var Shop = function(title){
+    this.title = title;
+    if(title === undefined){
+        this.title = "My Shop";
+    }
+    this.inventory = [];
+    this.loadInvetory = function(count){
+        for (var i = 1; i <= count; i++){
+            this.inventory.push(utils.generateItem(i));
+        }
+    }
+
+    this.loadInvetory(10);
+}
+
+var Item = function(){
+    this.id = "";
+    this.name = "";
+    this.price = "";
+}
+
+var utils = {
+    generateItem : function(id){
+        var item = new Item();
+        item.id = id;
+        item.name = faker.commerce.productName();
+        item.price = faker.commerce.price();
+        return item;
+    },
+    displayShop : function(shop){
+        if(shop instanceof Shop){
+            console.log("==========================");
+            console.log("Welcome to " + shop.title);
+            console.log("==========================");
+            for(var i = 0; i < shop.inventory.length; i++){
+                console.log(shop.inventory[i].id + ". " 
+                + shop.inventory[i].name + " - $" + shop.inventory[i].price);
+            }
+        }else{
+            console.log("Wrong Shop Object!");
+        }
+    }
+    
+}
+
+var alecxisShop = new Shop("Alecxis Shop");
+
+utils.displayShop(alecxisShop);
+
+console.log();
+console.log(cat());
