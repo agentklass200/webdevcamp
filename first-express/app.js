33483caa6477ae5c1cba71a42ab7cec61d48ccd2
Alecxis <alecxisbanag@hotmail.com>
2017-06-20 17:29:30 +0800

    Add color to console debug on first-express

--- first-express/app.js
+++ first-express/app.js
@@ -1,16 +1,17 @@
 var express = require("express");
 var app = express();
+var chalk = require("chalk");
 
 app.get("/", function(req, res){
-    res.send("<a href =\"/alecxis\">Alecxis</a>");
-    console.log("open \"/\"");
+    res.send("<a href =\"/alecxis\">Pogi</a>");
+    console.log(chalk.yellow('GET:') + " /");
 });
 
 app.get("/alecxis", function(req, res){
     res.send("<a href =\"\\..\">Go back!</a>");
-    console.log("open \"/alecxis\"");
+    console.log(chalk.yellow('GET:') + " /alecxis");
 })
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
-    console.log("Server has started!");
+    console.log(chalk.green('Server Has Started!'));
 });
